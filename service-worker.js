d2cf53e000582b4425d23e272abba84f4a57b117
Emre can Karacaağaçlı <scourge374@unknown7ed871e69d83.home>
2026-05-18 01:19:23 +0300

    Add PWA support

---
+++ service-worker.js
@@ -0,0 +1,55 @@
+const CACHE_NAME = "kahve-carki-v1";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(APP_SHELL);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys
+          .filter(function(key){
+            return key !== CACHE_NAME;
+          })
+          .map(function(key){
+            return caches.delete(key);
+          })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", function(event){
+  if(event.request.method !== "GET"){
+    return;
+  }
+
+  if(event.request.mode === "navigate"){
+    event.respondWith(
+      fetch(event.request).catch(function(){
+        return caches.match("./index.html");
+      })
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then(function(cachedResponse){
+      return cachedResponse || fetch(event.request);
+    })
+  );
+});
